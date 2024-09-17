01db5c2038672afd9d8ef39eb865c12683100669
Emir Gumrukcuoglu <58182762+gumrukcuoglu@users.noreply.github.com>
2024-09-17 16:45:06 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='interpolatorch',
+      version='0.1',
+      description='Simple interpolator for pytorch',
+      url='http://github.com/gumrukcuoglu/interpolatorch',
+      author='Emir Gumrukcuoglu',
+      author_email='emiramca@gmail.com',
+      license='GPL',
+      packages=['interpolatorch'],
+      zip_safe=False)
