7cca4a70f915baf32f7f0649d69cb0b462c696fa
Emir Gumrukcuoglu <58182762+gumrukcuoglu@users.noreply.github.com>
2024-09-18 16:14:34 +0100

    Update setup.py
    Version v0.2

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='interpolatorch',
-      version='0.1',
+      version='0.2',
       description='Simple interpolator for pytorch',
       url='http://github.com/gumrukcuoglu/interpolatorch',
       author='Emir Gumrukcuoglu',
